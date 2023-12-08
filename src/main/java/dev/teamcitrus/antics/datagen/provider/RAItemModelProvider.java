3756aaecc22f98541135c6d68d39797e9ef1c98b
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2023-12-08 01:15:53 +0000

    Ant Spawn Egg
    Why didn't I do this sooner

--- src/main/java/dev/teamcitrus/antics/datagen/provider/RAItemModelProvider.java
+++ src/main/java/dev/teamcitrus/antics/datagen/provider/RAItemModelProvider.java
@@ -6,7 +6,6 @@ import dev.teamcitrus.antics.registry.ItemRegistry;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -28,13 +27,19 @@ public class RAItemModelProvider extends ItemModelProvider {
         basicItem(BlockRegistry.GREAT_HEMLOCK_HANGING_SIGN.get().asItem());
         basicItem(BlockRegistry.PINECONE_BLOCK.get().asItem());
         basicItem(BlockRegistry.EMPTY_PINECONE_BLOCK.get().asItem());
+        basicItem(BlockRegistry.PINECONE_GARDEN.get().asItem());
         basicItem(BlockRegistry.BRANCH.get().asItem());
         basicItem(ItemRegistry.ANT_CHITIN.get());
+        spawnEggItem(ItemRegistry.ANT_SPAWN_EGG.get());
     }
 
-    public ItemModelBuilder controlledTextureBasicItem(Item item, String folder) {
-        return getBuilder(item.toString())
+    public void controlledTextureBasicItem(Item item, String folder) {
+        getBuilder(item.toString())
                 .parent(new ModelFile.UncheckedModelFile("item/generated"))
                 .texture("layer0", new ResourceLocation(ForgeRegistries.ITEMS.getKey(item).getNamespace(), folder + "/" + ForgeRegistries.ITEMS.getKey(item).getPath()));
+    }
+
+    public void spawnEggItem(Item item) {
+        getBuilder(item.toString()).parent(new ModelFile.UncheckedModelFile("minecraft:item/template_spawn_egg"));
     }
 }
--- src/main/java/dev/teamcitrus/antics/registry/ItemRegistry.java
+++ src/main/java/dev/teamcitrus/antics/registry/ItemRegistry.java
@@ -2,6 +2,7 @@ package dev.teamcitrus.antics.registry;
 
 import dev.teamcitrus.antics.Antics;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,6 +13,7 @@ public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Antics.MODID);
 
     public static final RegistryObject<Item> ANT_CHITIN = register("ant_chitin", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ANT_SPAWN_EGG = register("ant_spawn_egg", () -> new ForgeSpawnEggItem(EntityRegistry.ANT, 0x7B3F00, 0x6F4E37, new Item.Properties()));
 
     public static RegistryObject<Item> register(String name, Supplier<Item> supplier) {
         return ITEMS.register(name, supplier);
